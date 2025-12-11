7acfa25aab32f4ced4206b9261338ab0cf434db5
Rehan Raza <sydrehanraza@gmail.com>
2025-12-12 02:55:20 +0530

    DFF

--- vite.config.js
+++ vite.config.js
@@ -16,12 +16,12 @@ export default defineConfig({
         theme_color: '#0f172a',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
